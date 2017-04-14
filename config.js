4850ae9b971671a9ae808ca6391d565f56a7d37b
Fareed Javed <fareedjaved@gmail.com>
2017-04-14 09:34:54 -0400

    updated env variables

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 
-var cfg = {}; 
+var cfg = {};
 
-cfg.account_sid = process.env.TWILIO_ACCOUNT_SID; 
+// storing environment variables
+cfg.account_sid = process.env.TWILIO_ACCOUNT_SID;
 cfg.auth_token = process.env.TWILIO_AUTH_TOKEN;
 cfg.twil_num = process.env.TWILIO_NUM;
+cfg.my_num = process.env.MY_NUMBER;
 
 // exporting the configuration object
 module.exports = cfg;
